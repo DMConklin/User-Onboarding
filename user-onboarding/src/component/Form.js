638eb9727b903d271685b6ad9a17707eda6d8ddc
Darrell-Conklin <dmconklin84@gmail.com>
2020-03-18 22:44:09 -0500

    created boiler plate form

--- user-onboarding/src/component/Form.js
+++ user-onboarding/src/component/Form.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Form = () => {
     return(
-        <div></div>
+        <form>
+            <label htmlFor="name">Name</label>
+            <input name="name" id="name" required /><br />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" required /><br />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" required /><br />
+            <input type="checkbox" name="tos" id="tos" required />
+            <label htmlFor="tos">I Agree to Terms of Service</label><br />
+            <button>Submit</button>
+        </form>
     )
 }
 
